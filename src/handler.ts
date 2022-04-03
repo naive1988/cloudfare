59cfd45bd83bb77ff15299c47c61896f07f40827
naive1988 <1978253505@qq.com>
2022-04-03 21:00:15 +0800

    返回json

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,9 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  return new Response(JSON.stringify({ data: 'hello ' + request.url }), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json; charset=utf-8',
+      "Access-Control-Allow-Origin": "*"
+    }
+  })
 }
